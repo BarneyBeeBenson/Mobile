dbfd3258d247aac1a44fcb587e0391cc8efb6b1a
Douglas E. K <douglas.k2005@aluno.ifsc.edu.br>
2024-10-07 11:04:33 -0300

    Listagem simples
    terminado

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     String[] nomes = new String[] {"Davy Jones", "Jack Sparrow", "orje Barbosa", "Jonh Butstrap"};
@@ -21,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, nomes);
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),Integer.toString(i)+nomes[i], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
